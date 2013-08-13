aa603d17a2195a331d338c3f560980ad6898f05d
cyberchimps <cyberchimps@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-13 14:21:29 +0000

    name change and version number removed decimal
    git-svn-id: http://plugins.svn.wordpress.org/responsive-add-ons/trunk@755641 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- responsive-add-ons.php
+++ responsive-add-ons.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: Responsive Addons
+Plugin Name: Responsive Add Ons
 Plugin URI: http://cyberchimps.com
 Description: Added functionality for the responsive theme
-Version: 1.0.0.0
+Version: 1.0.0
 Author: CyberChimps
 Author URI: http://www.cyberchimps.com
 License: GPL2
