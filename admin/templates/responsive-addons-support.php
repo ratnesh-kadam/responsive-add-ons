9bd88ce1b945cc512b5902e9b70ab68c87e6b457
Ratnesh Kadam <ratnesh.kadam@hbwsl.com>
2019-09-09 16:51:48 +0530

    Fix - chnaged request support link

--- admin/templates/responsive-addons-support.php
+++ admin/templates/responsive-addons-support.php
@@ -17,7 +17,7 @@
 			<h3><?php esc_html_e( 'Premium Support' ); ?></h3>
 			<p><?php esc_html_e( 'Free Responsive user? Use the WordPress.org support forums to get help from our experts and other Responsive users.' ); ?></p>
 			<?php if ( is_resp_pro_license_is_active() ) { ?>
-				<a href="https://cyberchimps.com/my-account/support/" target="_blank"><?php esc_html_e( 'Request Support' ); ?></a>
+				<a href="https://cyberchimps.com/my-account/orders/?utm_source=responsive-add-ons&utm_medium=request-support&utm_campaign=responsive-support" target="_blank"><?php esc_html_e( 'Request Support' ); ?></a>
 			<?php } else { ?>
 				<a href="https://cyberchimps.com/responsive-pricing/?utm_source=responsive-add-ons&utm_medium=upgrade-to-responsive-pro&utm_campaign=upgrade-to-responsive-pro" target="_blank"><?php esc_html_e( 'Upgrade To Responsive Pro' ); ?></a>
 			<?php } ?>
