4b2b4ec867cc50e8cc8c000e9813a61dcedd8618
Ratnesh Kadam <ratnesh.kadam@hbwsl.com>
2020-02-11 14:49:36 +0530

    Feature - grunt task to create build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,8 @@
 /**
  * Grunt Tasks JavaScript.
  *
- * @package    Responsive Ready Sites Importer
+ * @package    Responsive_Add_Ons
+ * @subpackage Responsive_Add_Ons
  * @author     CyberChimps <https://www.cyberchimps.com>
  */
 
@@ -27,21 +28,29 @@ module.exports = function (grunt) {
 						'**',
 						'!node_modules/**',
 						'!vendor/**',
-						'!config/**',
 						'!release/**',
 						'!build/**',
+						'!bin/**',
+						'!tests/**',
 						'!.git/**',
 						'!Gruntfile.js',
 						'!package.json',
 						'!package-lock.json',
+						'!codeception.dist.yml',
+						'!.env.testing',
 						'!.gitignore',
 						'!.gitmodules',
+						'!.gitattributes',
 						'!composer.lock',
 						'!composer.json',
-						'!.env.testing',
-						'!tests/**',
+						'!phpunit.xml.dist',
+						'!.travis.xml',
+						'!.distignore',
+						'!.editorconfig',
+						'!.phpcs.xml.dist',
+						'!README.md',
+						'!config/**',
 						'!scripts/**',
-						'!codeception.dist.yml'
 					],
 					dest: 'release/<%= pkg.version %>/'
 				}
@@ -50,7 +59,7 @@ module.exports = function (grunt) {
 				build: {
 					options: {
 						mode: 'zip',
-						archive: './release/<%= pkg.name %>.<%= pkg.version %>.zip'
+						archive: './release/<%= pkg.name %>.zip'
 					},
 					expand: true,
 					cwd: 'release/<%= pkg.version %>/',
@@ -74,7 +83,7 @@ module.exports = function (grunt) {
 			wp_readme_to_markdown: {
 				your_target: {
 					files: {
-						'README.md': 'README.txt'
+						'README.md': 'readme.txt'
 					}
 				},
 			},
@@ -95,64 +104,6 @@ module.exports = function (grunt) {
 					}
 				}
 			},
-			uglify: {
-				options: {
-
-				},
-				admin: {
-					files: [{
-						expand: true,
-						cwd: 'release/<%= pkg.version %>/assets/js/admin/',
-						src: [
-							'*.js',
-							'!*.min.js'
-						],
-						dest: 'release/<%= pkg.version %>/assets/js/admin/',
-						ext: '.min.js'
-					}]
-				},
-				frontend: {
-					files: [{
-						expand: true,
-						cwd: 'release/<%= pkg.version %>/assets/js/',
-						src: [
-							'*.js',
-							'!*.min.js'
-						],
-						dest: 'release/<%= pkg.version %>/assets/js/',
-						ext: '.min.js'
-					}]
-				},
-			},
-			cssmin: {
-				options: {
-
-				},
-				admin: {
-					files: [{
-						expand: true,
-						cwd: 'release/<%= pkg.version %>/assets/css/admin/',
-						src: [
-							'*.css',
-							'!*.min.css'
-						],
-						dest: 'release/<%= pkg.version %>/assets/css/admin/',
-						ext: '.min.css'
-					}]
-				},
-				frontend: {
-					files: [{
-						expand: true,
-						cwd: 'release/<%= pkg.version %>/assets/css/',
-						src: [
-							'*.css',
-							'!*.min.css'
-						],
-						dest: 'release/<%= pkg.version %>/assets/css/',
-						ext: '.min.css'
-					}]
-				},
-			},
 		}
 	);
 
@@ -163,10 +114,10 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks( 'grunt-contrib-compress' );
 	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
 	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
-	grunt.registerTask( 'default', ['i18n'] );
+	grunt.registerTask( 'default', ['readme', 'build'] );
 	grunt.registerTask( 'i18n', ['addtextdomain', 'makepot'] );
 	grunt.registerTask( 'readme', ['wp_readme_to_markdown'] );
-	grunt.registerTask( 'build', ['clean:build', 'copy:build', 'uglify:admin', 'uglify:frontend', 'cssmin:admin', 'cssmin:frontend', 'compress:build'] );
+	grunt.registerTask( 'build', ['clean:build', 'copy:build', 'compress:build'] );
 
 	grunt.util.linefeed = '\n';
 
