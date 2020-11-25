15f67507918cec85b8f9db1a83089231db3d3ef6
Ratnesh Kadam <ratnesh.kadam@hbwsl.com>
2020-11-25 19:22:27 +0530

    Feature - Stop distributing the third party premium plugins

--- admin/partials/responsive-ready-sites-admin-display.php
+++ admin/partials/responsive-ready-sites-admin-display.php
@@ -189,13 +189,15 @@
 								<span class="responsive-ready-sites-tooltip-icon responsive-ready-sites-tooltip-plugins-settings" data-tip-id="responsive-ready-sites-tooltip-plugins-settings"><span class="dashicons dashicons-arrow-down-alt2"></span></span>
 								<div class="responsive-ready-sites-tooltip-message" id="responsive-ready-sites-tooltip-plugins-settings" style="display: none;">
 									<# if ( 'pro' === data.demo_type ) { #>
-										<div class="responsive-ready-sites-third-party-plugins-warning"><?php _e( 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', 'responsive-addons' ); ?></div>
+										<div class="responsive-ready-sites-third-party-plugins-warning"><?php _e( 'This ready site requires premium plugins. As these are third party premium plugins, you\'ll need to purchase, install and activate.', 'responsive-addons' ); ?></div>
 									<# } #>
-									<div class="responsive-ready-sites-plugins-installed"><?php _e( 'The plugin is already installed', 'responsive-addons' ); ?></div>
-									<div class="responsive-ready-sites-plugins-can-be-installed"><?php _e( 'We will install the plugin for you.', 'responsive-addons' ); ?></div>
-									<# if ( 'pro' === data.demo_type ) { #>
-										<div class="responsive-ready-sites-third-party-plugins"><?php _e( 'Premium Pluging - won\'t be installed.', 'responsive-addons' ); ?></div>
-									<# } #>
+                                    <div class="responsive-ready-sites-plugin-installation-info">
+									    <div class="responsive-ready-sites-plugins-installed"><?php _e( 'The plugin is already installed.', 'responsive-addons' ); ?></div>
+									    <div class="responsive-ready-sites-plugins-can-be-installed"><?php _e( 'We will install the plugin for you.', 'responsive-addons' ); ?></div>
+									    <# if ( 'pro' === data.demo_type ) { #>
+										    <div class="responsive-ready-sites-third-party-plugins"><?php _e( 'Premium Plugins - won\'t be installed.', 'responsive-addons' ); ?></div>
+									    <# } #>
+                                    </div>
 									<ul class="required-plugins-list"><span class="spinner is-active"></span></ul>
 								</div>
 							</li>
